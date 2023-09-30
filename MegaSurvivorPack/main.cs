dbf6204a8fae8e675874ecf5ab5cbb6c3a66761c
Quacker <49299629+Quacker0@users.noreply.github.com>
2023-09-30 16:52:41 +1000

    base git

---
+++ MegaSurvivorPack/main.cs
@@ -0,0 +1,229 @@
+﻿using System.Collections.Generic;
+using BepInEx;
+using KinematicCharacterController;
+using R2API;
+using R2API.Utils;
+using RoR2;
+using UnityEngine;
+
+
+
+
+namespace MegaSurvivorPack
+{
+  [BepInDependency(R2API.R2API.PluginGUID)]
+
+  [BepInPlugin(PluginGUID, PluginName, PluginVersion)]
+
+  //why you reading this my code is shit
+
+  public class SurvivorPlugin : BaseUnityPlugin
+  {
+    public const string PluginGUID = PluginAuthor + "." + PluginName;
+    public const string PluginAuthor = "Quacker";
+    public const string PluginName = "MegaSurvivorPack";
+    public const string PluginVersion = "1.0.0";
+    public const bool resize = false;
+        
+
+        public void Awake()
+    {
+      base.Logger.LogWarning("MegaSurvivorPack is loading...");
+
+      
+      On.RoR2.CharacterSelectBarController.Build += delegate(On.RoR2.CharacterSelectBarController.orig_Build orig, CharacterSelectBarController self)
+			{
+				self.iconContainerGrid.cellSize = new Vector2(64f, 64f);
+				self.iconContainerGrid.constraintCount = 17;
+				orig.Invoke(self);
+        
+			};
+      base.Logger.LogWarning("MegaSurvivorPack : CharacterSelectBar eddited");
+
+      
+      var survivorlistdef = new List<survivorinfo>(){
+        new survivorinfo() {name = "paladin", dlcIdentifier = false, flyer = false},
+        new survivorinfo() {name = "golem", dlcIdentifier = false, flyer = false, modelsize = 0.805f,CapY=-2.0f,camV= 1.4f},
+        new survivorinfo() {name = "hand", dlcIdentifier = false, flyer = false},
+        new survivorinfo() {name = "beetleGuard", dlcIdentifier = false, flyer = false},
+        new survivorinfo() {name = "magmaWorm", dlcIdentifier = false, flyer = false},
+        new survivorinfo() {name = "electricWorm", dlcIdentifier = false, flyer = false},
+        new survivorinfo() {name = "impBoss", dlcIdentifier = false, flyer = false},
+        new survivorinfo() {name = "titangold", dlcIdentifier = false, flyer = false},
+        new survivorinfo() {name = "beetle", dlcIdentifier = false, flyer = false},
+        new survivorinfo() {name = "bison", dlcIdentifier = false, flyer = false},
+        new survivorinfo() {name = "megaDrone", dlcIdentifier = false, flyer = true},
+        new survivorinfo() {name = "drone1", dlcIdentifier = false, flyer = true},
+        //error new survivorinfo() {name = "flamedrone", dlcIdentifier = false, flyer = true},
+        new survivorinfo() {name = "MissileDrone", dlcIdentifier = false, flyer = true},
+        new survivorinfo() {name = "BackupDrone", dlcIdentifier = false, flyer = true},
+        new survivorinfo() {name = "greaterWisp", dlcIdentifier = false, flyer = true},
+        new survivorinfo() {name = "imp", dlcIdentifier = false, flyer = false},
+        new survivorinfo() {name = "lemurianBruiser", dlcIdentifier = false, flyer = false},
+        new survivorinfo() {name = "titan", dlcIdentifier = false, flyer = false},
+        new survivorinfo() {name = "bell", dlcIdentifier = false, flyer = true},
+        new survivorinfo() {name = "clayBruiser", dlcIdentifier = false, flyer = false},
+        //error new survivorinfo() {name = "roboBallBoss", dlcIdentifier = false, flyer = true},
+        //error new survivorinfo() {name = "superRoboBallBoss", dlcIdentifier = false, flyer = true},
+        new survivorinfo() {name = "vulture", dlcIdentifier = false, flyer = true},
+        new survivorinfo() {name = "archWisp", dlcIdentifier = false, flyer = true},
+        new survivorinfo() {name = "nullifier", dlcIdentifier = false, flyer = false},
+        new survivorinfo() {name = "scav", dlcIdentifier = false, flyer = false},
+        new survivorinfo() {name = "gravekeeper", dlcIdentifier = false, flyer = false},
+        new survivorinfo() {name = "parent", dlcIdentifier = false, flyer = false},
+        new survivorinfo() {name = "Heretic", dlcIdentifier = false, flyer = false},
+        new survivorinfo() {name = "minimushroom", dlcIdentifier = false, flyer = false},
+        new survivorinfo() {name = "Brother", dlcIdentifier = false, flyer = false,modelsize = 3.105f,CapY=-3.8f,camV= -7.0f},
+        new survivorinfo() {name = "BrotherGlass", dlcIdentifier = false, flyer = false},
+        new survivorinfo() {name = "BrotherHurt", dlcIdentifier = false, flyer = false},
+
+      };
+
+      addsurvivors(survivorlistdef);
+
+      base.Logger.LogWarning("MegaSurvivorPack is loaded!");
+    }
+
+    public void addsurvivors(List<survivorinfo> survivorlist)
+    {
+      
+      foreach (survivorinfo i in survivorlist)
+      {
+        
+        GameObject bodycopy = PrefabAPI.InstantiateClone(Resources.Load<GameObject>("prefabs/characterbodies/" + i.name + "Body"), i.name);
+        //GameObject bodycopy2 = PrefabAPI.InstantiateClone(Resources.Load<GameObject>("prefabs/characterbodies/" + i.name + "Body"), i.name);
+
+        
+        GameObject finalcopy = bodycopy.GetComponent<ModelLocator>().modelTransform.gameObject;
+
+                
+                    finalcopy.transform.localScale -= new Vector3(i.modelsize, i.modelsize, i.modelsize);
+                    //bodycopy.transform.localScale -= new Vector3(i.modelsize, i.modelsize, i.modelsize);
+
+                    if (i.flyer)
+                    {
+                        //CharacterMotor flycontroler = bodycopy.GetComponent<CharacterMotor>();
+
+
+                    }
+
+                    if (!i.flyer)
+                    {
+                    if (resize)
+                    {
+                        KinematicCharacterMotor landcontroler = bodycopy.GetComponent<KinematicCharacterMotor>();
+
+
+
+                        float OrgCapsuleRadius = landcontroler.CapsuleRadius;
+
+
+                        float OrgCapsuleHeight = Reflection.GetFieldValue<float>(landcontroler, "CapsuleHeight");
+                        float OrgCapsuleYOffset = Reflection.GetFieldValue<float>(landcontroler, "CapsuleYOffset");
+
+                        //base.Logger.LogError("org radius | " + OrgCapsuleRadius.ToString());
+                        //base.Logger.LogError("org height | " + OrgCapsuleHeight.ToString());
+                        base.Logger.LogError("org r | " + i.name + " : " + OrgCapsuleRadius.ToString());
+                        base.Logger.LogError("org h | " + i.name + " : " + OrgCapsuleHeight.ToString());
+                        base.Logger.LogError("org y | " + i.name + " : " + OrgCapsuleYOffset.ToString());
+
+                        Reflection.SetFieldValue<float>(landcontroler, "CapsuleRadius", OrgCapsuleRadius = 0.74f);
+                        Reflection.SetFieldValue<float>(landcontroler, "CapsuleHeight", OrgCapsuleHeight = 2.14f);
+                        Reflection.SetFieldValue<float>(landcontroler, "CapsuleYOffset", OrgCapsuleYOffset = i.CapY);
+
+                        float NewCapsuleRadius = Reflection.GetFieldValue<float>(landcontroler, "CapsuleRadius");
+                        float NewCapsuleHeight = Reflection.GetFieldValue<float>(landcontroler, "CapsuleHeight");
+                        float NewCapsuleYOffset = Reflection.GetFieldValue<float>(landcontroler, "CapsuleYOffset");
+
+                        //base.Logger.LogError("new radius  | " + NewCapsuleRadius.ToString());
+                        //base.Logger.LogError("new height  | " + NewCapsuleHeight.ToString());
+                        base.Logger.LogError("new y  | " + i.name + " : " + NewCapsuleRadius.ToString());
+                        base.Logger.LogError("new y  | " + i.name + " : " + NewCapsuleHeight.ToString());
+                        base.Logger.LogError("new y  | " + i.name + " : " + NewCapsuleYOffset.ToString());
+
+
+
+
+                        //camerapram.RemoveParamsOverride(handle);
+
+
+
+
+
+
+
+                        //CameraTargetParams camerapram = gameObject.GetComponent<CameraTargetParams>();
+                        //camerapram.cameraParams.pivotVerticalOffset -= 0.81f;
+
+                    }
+                    };
+                
+
+                
+                Interactor interact = bodycopy.GetComponent<Interactor>();
+                interact.maxInteractionDistance = 6f;
+
+
+                CameraTargetParams camerapram = bodycopy.GetComponent<CameraTargetParams>();
+
+                camerapram.cameraParams = ScriptableObject.CreateInstance<CharacterCameraParams>();
+                var copy = LegacyResourcesAPI.Load<GameObject>("Prefabs/CharacterBodies/CommandoBody").GetComponent<CameraTargetParams>().cameraParams;
+
+
+                camerapram.cameraParams.data.pivotVerticalOffset = 0;//i.camV;
+                camerapram.cameraParams.name = "quackcam " + i.name;
+                camerapram.cameraParams.data.maxPitch = copy.data.maxPitch;
+                camerapram.cameraParams.data.minPitch = copy.data.minPitch;
+                camerapram.cameraParams.data.wallCushion = copy.data.wallCushion;
+                camerapram.cameraParams.data.idealLocalCameraPos = new Vector3(0f, i.camV, -10f);
+
+                camerapram.cameraPivotTransform = null;
+            
+                camerapram.recoil = Vector2.zero;
+                
+                camerapram.dontRaycastToPivot = false;
+
+                
+
+
+
+                SurvivorDef newsurvivor = ScriptableObject.CreateInstance<SurvivorDef>();
+				newsurvivor.bodyPrefab = bodycopy;
+				newsurvivor.bodyPrefab.GetComponent<CharacterBody>().preferredPodPrefab = Resources.Load<GameObject>("Prefabs/NetworkedObjects/SurvivorPod");
+				newsurvivor.descriptionToken = i.name + ": MegaSurvivorPack \n";
+				newsurvivor.displayPrefab = finalcopy;
+                newsurvivor.cachedName = i.name;
+        
+
+				newsurvivor.primaryColor = new Color(UnityEngine.Random.Range(0f, 1f), UnityEngine.Random.Range(0f, 1f), UnityEngine.Random.Range(0f, 1f));
+				base.Logger.LogMessage(i.name + " added ");
+				
+                R2API.ContentAddition.AddBody(newsurvivor.bodyPrefab);
+				R2API.ContentAddition.AddSurvivorDef(newsurvivor);
+
+        
+      }
+      
+    
+      
+
+    }
+
+    
+  }
+
+  
+public class survivorinfo
+{
+  public string name = "quack";
+  public bool dlcIdentifier = false;
+  public bool flyer = false;
+
+  public float modelsize = 0f;
+  public float CapR = 0f;
+  public float CapH = 0f;
+  public float CapY = 0f;
+  public float camV = 0f;
+}
+  
+}
---
+++ README.md
@@ -0,0 +1,91 @@
+Mega Survivor Pack
+
+My discord https://discord.gg/rA7MyXSgjR
+
+
+WARNING Most of the "Survivors" are hard to use or impossible to survive very long with and with the larger "Survivors" most of the time they can’t open chests i have somewhat tried to fix this but it is still a problem
+
+
+
+
+This mod currently lets you play as most of the enemies in the game and some of the bosses (this list is a little outdated)
+- Bandit
+- Sniper
+- Hand
+- Beetle Guard
+- Magma Worm
+- Overloading Worm	
+- Imp Overlord
+- Aurelionite
+- Beetle
+- Bighorn Bison
+- TC-280 Prototype
+- Stone Golem
+- Greater Wisp
+- Imp
+- Elder Lemurian
+- Lemurian
+- Stone Titan
+- Paladin
+- Bell
+- ClayBruiser
+- RoboBallBoss
+- Vulture
+
+Bandit's "R" currently is very bad
+Sniper's model is a untextured commando and icon in survivor select is the commando and name is "???"
+Paladin's model is a untextured commando and name is commando in the survivor screen
+
+I do not know if i will continue to update this mod the source is in git feel free to pull request and il update the mod
+(Note i have post build events in the csproj)
+
+_________________ NEW Changelog ---------
+version 1.0.0
+- recreated mod from scratch
+
+
+
+----------------- OLD Changelog ---------
+version 4.0.2
+- fixed latest version
+version 4.0.1
+- added survivors updated to latest game version
+
+version 4.0.0
+- added survivors updated to latest game version
+
+version 3.5.0
+- major survivor changes in beta(can change by going to the config)
+- added new survivors will update list next update
+
+version 3.4.1
+- patch
+
+version 3.4.0
+- patch
+- added experimental mode (activate in config)
+
+version 3.3.1
+- patch
+
+version 3.3 
+- fix for skills 2.0 update
+- added scroll function (left and right arrow keys and down to reset)
+- added Paladin, Bell, ClayBruiser, RoboBallBoss, Vulture
+
+Version 3.2.2
+- fix for Scorched Acres
+- removed Hermit crab so that all survivors can fit on screen
+
+Version 3.2.1
+- changed how it was adding survivors
+- removed dev tools
+
+
+Version 3.1
+- added Sniper
+
+
+Version 3.0
+- Updated to use BepInEx
+- added overloading worm
